86ebab5c287af11c5a29f273bf222a9914762782
Jordi Noguera <jordinoguera83+github@gmail.com>
2020-07-08 18:13:19 +0200

    Fix generation of SVGs

--- scripts/exportSvgs.js
+++ scripts/exportSvgs.js
@@ -1,13 +1,13 @@
 import { readFileSync, writeFileSync } from 'fs'
 import { join } from 'path'
-import loadJSON from '../src/helpers/loadJSON'
+import loadData from '../src/loadData'
 
-const { radars } = loadJSON('radars.json')
+loadData('radars.json').then(({ radars }) => {
+  radars.forEach(radar => {
+    const inputPath = join(process.cwd(), 'out', `${radar.slug}.html`)
+    const svg = readFileSync(inputPath, 'utf-8').match(/<svg.*<\/svg>/)[0]
 
-radars.forEach(radar => {
-  const inputPath = join(process.cwd(), 'out', `${radar.id}.html`)
-  const svg = readFileSync(inputPath, 'utf-8').match(/<svg.*<\/svg>/)[0]
-
-  const outputPath = join(process.cwd(), 'out', `${radar.id}.svg`)
-  writeFileSync(outputPath, svg)
+    const outputPath = join(process.cwd(), 'out', `${radar.slug}.svg`)
+    writeFileSync(outputPath, svg)
+  })
 })
