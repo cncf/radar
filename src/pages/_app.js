5b2e53b2a3a22fb43bcc41f9b3c373a96e8818e2
Jordi Noguera <jordinoguera83+github@gmail.com>
2020-07-10 10:51:38 +0200

    Add navigation

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,7 @@
-import "bulma/css/bulma.css"
-import { Fragment } from "react";
-import Head from "next/head";
+import 'bulma/css/bulma.css'
+import { Fragment } from 'react'
+import Head from 'next/head';
+import Link from 'next/link'
 
 export default ({ Component, pageProps }) => {
   return <Fragment>
@@ -9,6 +10,23 @@ export default ({ Component, pageProps }) => {
       <meta name="viewport" content="width=device-width, initial-scale=1"/>
       <link rel="icon" href="/favicon.ico"/>
     </Head>
+
+    <div className="container is-fluid">
+      <nav className="navbar" role="navigation" aria-label="main navigation">
+        <div className="navbar-brand">
+          <Link href="/"><a className="navbar-item"><h1 className="title">CNCF Radars</h1></a></Link>
+        </div>
+
+        <div className="navbar-menu">
+          <div className="navbar-end">
+            <div className="navbar-item">
+              <Link href="/overview"><a>Overview</a></Link>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </div>
+
     <Component {...pageProps} />
   </Fragment>
 }
