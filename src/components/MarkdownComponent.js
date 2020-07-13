a9f5bf351360483df3106b83b217c78f76921a95
Jordi Noguera <jordinoguera83+github@gmail.com>
2020-07-13 17:49:35 +0200

    Break long lines in yaml markdown

--- src/components/MarkdownComponent.js
+++ src/components/MarkdownComponent.js
@@ -2,7 +2,7 @@ import { Converter } from 'showdown'
 import sanitizeHtml from 'sanitize-html'
 
 export default ({ value, ...props }) => {
-  const html = (new Converter({simpleLineBreaks: true})).makeHtml(value);
+  const html = (new Converter({simpleLineBreaks: false})).makeHtml(value);
   const sanitizedHtml = sanitizeHtml(html)
 
   return <div {...props} dangerouslySetInnerHTML={{ __html: sanitizedHtml }} />
