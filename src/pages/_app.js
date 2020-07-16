268ded170a9709fef979956a0b69e0faafaa19f1
Jordi Noguera <jordinoguera83+github@gmail.com>
2020-07-16 15:22:59 +0200

    Add favicon

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -12,7 +12,7 @@ export default ({ Component, pageProps }) => {
     <Head>
       <title>CNCF Radars</title>
       <meta name="viewport" content="width=device-width, initial-scale=1"/>
-      <link rel="icon" href="/favicon.ico"/>
+      <link rel="icon" href="/favicon.png"/>
     </Head>
 
     <SearchContext.Provider value={{searchQuery, setSearchQuery}}>
