ddf44c606e63e7c68e7579f7cf12e06e690219b9
Jordi Noguera <jordinoguera83+github@gmail.com>
2020-07-17 17:30:58 +0200

    Add definitions to homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,9 +2,16 @@ import Link from 'next/link'
 import { Fragment } from 'react'
 import loadData from '../loadData'
 import Section from '../components/Section'
+import LevelTag from "../components/LevelTag";
 
 export default function Home({ radars }) {
   return <Fragment>
+    <style jsx>{`
+      td {
+        padding-right: 20px;
+        padding-bottom: 20px;
+      }
+    `}</style>
     <Section>
       <h2>Published Radars</h2>
       <ul>
@@ -14,7 +21,24 @@ export default function Home({ radars }) {
 
     <Section>
       <h2>Definitions</h2>
-      <h3>TODO: Add definitions for Adopt, Trial, Assess</h3>
+
+      <table>
+        <tr>
+          <td><LevelTag level="adopt" className="is-medium" /></td>
+          <td>We can clearly recommend this technology. We have used it for long periods of time in many teams and it has proven to be stable and useful</td>
+        </tr>
+
+        <tr>
+          <td><LevelTag level="trial" className="is-medium" /></td>
+          <td>We have used it with success and recommend to have a closer look at the technology in this ring</td>
+        </tr>
+
+        <tr>
+          <td><LevelTag level="assess" className="is-medium" /></td>
+          <td>We have tried it out and we find it promising. We recommend having a look at these items when you face a
+            specific need for the technology in your project.</td>
+        </tr>
+      </table>
     </Section>
 
     <Section>
