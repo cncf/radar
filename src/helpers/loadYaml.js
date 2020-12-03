00257612d1cc2592ded87d60f4d18f0fa90b1a21
Jordi Noguera <jordinoguera83+github@gmail.com>
2020-12-03 14:19:56 +0100

    Throw more descriptive error message if YAML file can't be parsed

--- src/helpers/loadYaml.js
+++ src/helpers/loadYaml.js
@@ -3,6 +3,11 @@ const { readFileSync } = require('fs')
 const YAML = require('yaml')
 
 module.exports = (...path) => {
-  const fullPath = join(process.cwd(), 'content', ...path)
-  return YAML.parse(readFileSync(fullPath, 'utf-8'))
+  const relativePath = join('content', ...path)
+  const fullPath = join(process.cwd(), relativePath)
+  try {
+    return YAML.parse(readFileSync(fullPath, 'utf-8'))
+  } catch(e) {
+    throw new Error(`Malformed YAML file ${relativePath}`)
+  }
 }
