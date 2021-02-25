b972ec651b3dda56ae8c0debed8d0f8a4f3f1781
Jordi Noguera <jordinoguera83+github@gmail.com>
2021-02-25 11:38:06 +0100

    Link to homepage if point does not have associated landscape item

--- src/components/LinkToPoint.js
+++ src/components/LinkToPoint.js
@@ -1,7 +1,14 @@
 import { useContext } from 'react'
 import SelectedPointContext from '../contexts/SelectedPointContext'
+import OutboundLink from './OutboundLink'
 
 export default function LinkToPoint({ point }) {
   const { setSelectedPoint } = useContext(SelectedPointContext)
-  return <a onClick={_ => setSelectedPoint(point.landscapeId)}>{point.name}</a>
+
+  if (point.landscapeId) {
+    return <a onClick={_ => setSelectedPoint(point.landscapeId)}>{point.name}</a>
+  }
+
+  const href = point.homepage || `https://github.com/${point.repo}`
+  return <OutboundLink href={href}>{point.name}</OutboundLink>
 }
